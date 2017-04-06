c0dbec430e20a92656487bfffdaee4fbea2e9701
mrLysyi <cheburano@gmail.com>
2017-04-06 23:27:50 +0300

    front -  page routing

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+//import {} from "@angular/router"; //RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ManagerOrdersComponent } from './manager/manager-orders/manager-orders.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    HomePageComponent,
+    ManagerOrdersComponent
   ],
   imports: [
     BrowserModule,
