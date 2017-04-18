73a1bd534d1843d2434c7774d1c8203f9d680650
Anton <anton.pasichnyk@gmail.com>
2017-04-18 18:14:58 +0300

    Add demo version of 3D Configurator

---
+++ frontend/src/main/frontend/src/app/configurator/configurator.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Anton on 18-Apr-17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'configurator',
+  templateUrl: './configurator.component.html',
+  // styleUrls: ['./menu.component.css']
+})
+export class ConfiguratorComponent {
+
+}
---
+++ frontend/src/main/frontend/src/app/shared/safepipe.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Anton on 18-Apr-17.
+ */
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
