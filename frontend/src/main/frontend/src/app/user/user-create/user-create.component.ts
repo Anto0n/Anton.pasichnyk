e60892327b238df746c613f9a55d4c90d96a6def
mrLysyi <cheburano@gmail.com>
2017-04-25 23:43:00 +0300

    --||--

--- frontend/src/main/frontend/src/app/user/user-create/user-create.component.ts
+++ frontend/src/main/frontend/src/app/user/user-create/user-create.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
-import { UserCreateService } from 'user-create.service';
+//import { UserCreateService } from 'user-create.service';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.css'],
-  providers: [UserCreateService]
+  styleUrls: ['./user-create.component.css']
+  //providers: [UserCreateService]
 })
 export class UserCreateComponent  {
 
   users: User[] = [];
-  constructor (private userService: UserCreateService) {}
+ // constructor (private userService: UserCreateService) {}
   userCreate (firstName, lastName, login, email, password1, password2) {
     if (password1 === password2) {
      // new User  = new User(firstName, lastName, login, email, password1);
