c631afffaf770da0f9b0269711d2a751d26e0457
mrLysyi <cheburano@gmail.com>
2017-05-21 10:58:17 +0300

    bootstrap - need to config

--- frontend/src/main/frontend/src/app/services/order.service.ts
+++ frontend/src/main/frontend/src/app/services/order.service.ts
@@ -7,7 +7,7 @@ export class OrderService {
   // Observable string sources
   private orderCreateSource = new Subject<string>();
 
-// Observable string streams
+// Observable order streams
 
   // Service message commands
 //https://angular.io/docs/ts/latest/cookbook/component-communication.html#!#parent-to-child-on-changes
