4b6c93e1eacdebf721ac57422682c5d71b60c0ab
nico.tvu <nico.tvu@gmail.com>
2017-06-01 00:10:40 +0300

    Merge branch 'master_m' of C:\Users\nicot\IdeaProjects\cotton with conflicts.

--- backend/src/main/java/com/bionic/baglab/BaglabApplicationProd.java
+++ backend/src/main/java/com/bionic/baglab/BaglabApplicationProd.java
@@ -1,24 +1,24 @@
-package com.bionic.baglab;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-/**
- * Start point for production App (Tomcat 9)
- */
-
-
-/*
-
-@SpringBootApplication
-public class BaglabApplicationProd extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BaglabApplicationProd.class);
-    }
-}
-
-
-*/
+//package com.bionic.baglab;
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//
+///**
+// * Start point for production App (Tomcat 9)
+// */
+//
+//
+//
+//
+//@SpringBootApplication
+//public class BaglabApplicationProd extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(BaglabApplicationProd.class);
+//    }
+//}
+//
+//
+//
